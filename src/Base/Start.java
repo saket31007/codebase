154f5a20bd63e6bdb9e4ac8f9448753b135ebba1
Saket Barmate <saket.barmate@gmail.com>
2023-12-20 00:20:08 +0530

    another commit

--- src/Base/Start.java
+++ src/Base/Start.java
@@ -6,7 +6,49 @@ public class Start {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello World");
+		
+		sayHello("Saket Barmate");
+	
+		tablem(10, 2 );
+		
+		Start start = new Start();
+		
+		int sum =start.SumofThreeNum(50, 2, 2);
+		System.out.println(sum);
 
+	}
+	
+	public static String sayHello(String name)
+	{
+		System.out.println("Hello" +" Mr." + " "+ name.toUpperCase());
+		
+		return name;
+		
+		
+	} 
+	
+	public static int tablem(int i, int n) {
+		
+		int t = i*i;
+		
+		
+		for(int j =1; j <=i; j++) {
+		
+			System.out.println(n*j);
+		}
+		
+		System.out.println(t);
+		
+		return t;}
+	
+	
+	public static int SumofThreeNum(int firstNum , int SecondNum , int ThirdNum) {
+		
+		int sum =  firstNum +  SecondNum +  ThirdNum;
+		
+		return sum;
+		
+		
 	}
 
 }
